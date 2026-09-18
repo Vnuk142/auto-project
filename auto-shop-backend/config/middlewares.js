84f98f6cafcf8d500a204d10a0c58abda9c947f4
Vnuk142 <pyd5jsnzrr@privaterelay.appleid.com>
2026-09-18 19:25:12 +0700

    Update middlewares.js

--- auto-shop-backend/config/middlewares.js
+++ auto-shop-backend/config/middlewares.js
@@ -1,8 +1,26 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'https://auto-project-six.vercel.app'],
+          'media-src': ["'self'", 'data:', 'blob:', 'https://auto-project-six.vercel.app'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['https://auto-project-six.vercel.app'],
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
